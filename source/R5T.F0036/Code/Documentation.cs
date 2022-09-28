7d2424d50c985b6478bc78f69192a034ec353949
davidcoats <david@getsafetycone.com>
2022-09-28 10:05:06 -0700

    Initial commit.

---
+++ source/R5T.F0036/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.F0036
+{
+	/// <summary>
+	/// Service provider builder and services configure functionality.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.F0036/Code/Instances.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace R5T.F0036
+{
+    public static class Instances
+    {
+		
+    }
+}

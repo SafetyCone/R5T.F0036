8e9f944b9129556c34e7e5d2bc8162d0f921ef86
davidcoats <david@getsafetycone.com>
2022-10-10 22:28:28 -0700

    Interim changes.

---
+++ source/R5T.F0036.F001/Code/Types/Interfaces/IServicesConfigurer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace R5T.F0036.F001
+{
+    /// <summary>
+    /// Chooses <see cref="IAsynchronousServicesConfigurer"/> as the default services configurer.
+    /// </summary>
+    public interface IServicesConfigurer : IAsynchronousServicesConfigurer
+    {
+    }
+}

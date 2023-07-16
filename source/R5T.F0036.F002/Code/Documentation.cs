013038cc418d1f3249ddad37713b3970548eb4ad
davidcoats <david@getsafetycone.com>
2023-07-16 16:24:32 -0600

    Interim changes.

--- source/R5T.F0036.F002/Code/Documentation.cs
+++ source/R5T.F0036.F002/Code/Documentation.cs
@@ -4,7 +4,7 @@ using System;
 namespace R5T.F0036.F002
 {
 	/// <summary>
-	/// IServiceAction<T>-based services configuration functionality.
+	/// IServiceAction&lt;T&gt;-based services configuration functionality.
 	/// </summary>
 	public static class Documentation
 	{

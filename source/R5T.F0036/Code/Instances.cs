8bb7d334b072963935d65b12305ea08c7fbd8c9a
davidcoats <david@getsafetycone.com>
2024-08-27 22:53:19 -0700

    Interim commit.

--- source/R5T.F0036/Code/Instances.cs
+++ source/R5T.F0036/Code/Instances.cs
@@ -1,22 +1,19 @@
 using System;
 
-using R5T.F0036.F000;
-using R5T.F0036.F001;
-
 
 namespace R5T.F0036
 {
     public static class Instances
     {
-        public static IHasServicesOperator HasServicesOperator { get; } = F000.HasServicesOperator.Instance;
-        public static IServiceCollectionBuilderOperator ServiceCollectionBuilderOperator { get; } = F000.ServiceCollectionBuilderOperator.Instance;
-        public static IServiceCollectionOperator ServiceCollectionOperator { get; } = F000.ServiceCollectionOperator.Instance;
-        public static IServicesBuilderOperator ServicesBuilderOperator { get; } = F0036.ServicesBuilderOperator.Instance;
-        public static IServicesConfigurerOperator ServicesConfigurerOperator { get; } = F001.ServicesConfigurerOperator.Instance;
-        public static IServiceProviderBuilderOperator ServiceProviderBuilderOperator { get; } = F000.ServiceProviderBuilderOperator.Instance;
-        public static IServiceProviderOperator ServiceProviderOperator { get; } = F000.ServiceProviderOperator.Instance;
+        public static F000.IHasServicesOperator HasServicesOperator => F000.HasServicesOperator.Instance;
+        public static F000.IServiceCollectionBuilderOperator ServiceCollectionBuilderOperator => F000.ServiceCollectionBuilderOperator.Instance;
+        public static F000.IServiceCollectionOperator ServiceCollectionOperator => F000.ServiceCollectionOperator.Instance;
+        public static IServicesBuilderOperator ServicesBuilderOperator => F0036.ServicesBuilderOperator.Instance;
+        public static F001.IServicesConfigurerOperator ServicesConfigurerOperator => F001.ServicesConfigurerOperator.Instance;
+        public static F000.IServiceProviderBuilderOperator ServiceProviderBuilderOperator => F000.ServiceProviderBuilderOperator.Instance;
+        public static F000.IServiceProviderOperator ServiceProviderOperator => F000.ServiceProviderOperator.Instance;
 
-        public static IServiceProviderOperator ServiceProvider => F000.Instances.ServiceProvider;
-        public static IServiceCollectionOperator Services => F000.Instances.Services;
+        public static F000.IServiceProviderOperator ServiceProvider => F000.Instances.ServiceProvider;
+        public static F000.IServiceCollectionOperator Services => F000.Instances.Services;
     }
 }
